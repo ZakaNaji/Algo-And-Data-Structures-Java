4baf0adf669bfba368e9d7f82460eb86bf991c3f
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-09-28 14:50:00 +0100

    #Data Structure: Arrays(Operations) - removeEvenNumbers.

--- src/main/java/com/znaji/arrays/DifferentArrayOperations.java
+++ src/main/java/com/znaji/arrays/DifferentArrayOperations.java
@@ -4,12 +4,30 @@ public class DifferentArrayOperations {
 
         public static void main(String[] args) {
             int[] arr = new int[] {1,2,3,4,5,6,7,8,9,10};
-            printArray(arr);
+            printArray(removeEvenNumbers(arr));
         }
 
         public static void printArray(int[] arr) {
             for (int i = 0; i < arr.length; i++) {
                 System.out.println("Array element at index " + i + ": " + arr[i]);
             }
+        }
+
+        public static int[] removeEvenNumbers(int[] arr) {
+            int oddCount = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 != 0) {
+                    oddCount++;
+                }
+            }
+            int[] oddArr = new int[oddCount];
+            int j = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] % 2 != 0) {
+                    oddArr[j] = arr[i];
+                    j++;
+                }
+            }
+            return oddArr;
         }
 }
