79da832ca249aae3e0c3f5b89268291eb6798ad1
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-02 19:35:08 +0100

    Arrays - numeric functions: find 2nd max( this time we use to vars to track max and secondMax), the idea is check if current value > thatn max then update max with current value and secondMax with max, else if current value is btw max and secondMax we update secondMax to the current value.

--- src/main/java/com/znaji/arrays/js/ArrayNumFunctions.js
+++ src/main/java/com/znaji/arrays/js/ArrayNumFunctions.js
@@ -23,9 +23,22 @@ function findMax(arr) {
   }
   return max;
 }
+function findSecondMax(arr) {
+  let max = arr[0];
+  let secondMax = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] > secondMax && arr[i] < max) {
+      secondMax = arr[i];
+    }
+  }
+  return secondMax;
+}
 function main() {
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4];
-  console.log("max: " + findMax(arr));
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 9];
+  console.log("secondMax: " + findSecondMax(arr));
 }
 
 main();
