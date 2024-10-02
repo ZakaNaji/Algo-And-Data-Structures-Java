e6ff7b4d9da8e540d0ea29158bec1d09fe16ee86
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-02 19:19:55 +0100

    Arrays - numeric operations/functions: - reverse an array

--- src/main/java/com/znaji/arrays/js/ArrayNumFunctions.js
+++ src/main/java/com/znaji/arrays/js/ArrayNumFunctions.js
@@ -7,10 +7,19 @@ function removeEvenNumbers(arr) {
   }
   return newArr;
 }
+function reverseArray(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+}
 
 function main() {
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  let newArr = removeEvenNumbers(arr);
-  console.log(newArr);
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  console.log("Original Array: " + arr);
+  reverseArray(arr);
+  console.log("Reverse Array: " + arr);
 }
+
 main();
