9d877d1b332141ea7e9037dc0daa566c72c4013d
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-02 19:22:59 +0100

    Arrays - numeric functions: find max in num array.

--- src/main/java/com/znaji/arrays/js/ArrayNumFunctions.js
+++ src/main/java/com/znaji/arrays/js/ArrayNumFunctions.js
@@ -14,12 +14,18 @@ function reverseArray(arr) {
     arr[arr.length - 1 - i] = temp;
   }
 }
-
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
 function main() {
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  console.log("Original Array: " + arr);
-  reverseArray(arr);
-  console.log("Reverse Array: " + arr);
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4];
+  console.log("max: " + findMax(arr));
 }
 
 main();
