e76f79d652eec037c3718395c172a0da4f6fb012
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-10 20:11:13 +0100

    LinkedList - correct append implimetation + add pop implimentation.

--- src/main/java/com/znaji/LinkedList/MyLinkedList.js
+++ src/main/java/com/znaji/LinkedList/MyLinkedList.js
@@ -13,12 +13,36 @@ class MyLinkedList {
       value: value,
       next: null,
     };
-    this.head.next = newNode;
+    this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
+  }
+
+  pop() {
+    if (this.length < 2) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return;
+    }
+    let node = this.head;
+    console.log(node);
+    while (node.next.next != null) {
+      console.log(node);
+      node = node.next;
+    }
+    node.next = null;
+    this.tail = node;
+    this.length--;
   }
 }
 
 const myLinkedList = new MyLinkedList(1);
 myLinkedList.append(2);
+myLinkedList.append(3);
+myLinkedList.pop();
+console.log(myLinkedList);
+myLinkedList.pop();
+console.log(myLinkedList);
+myLinkedList.pop();
 console.log(myLinkedList);
