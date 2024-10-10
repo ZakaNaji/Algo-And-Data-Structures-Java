f0a564e26d16ba518f06adc5a8bfdf6e3f210279
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-10 19:49:03 +0100

    LinkedList - push function.

--- src/main/java/com/znaji/LinkedList/MyLinkedList.js
+++ src/main/java/com/znaji/LinkedList/MyLinkedList.js
@@ -4,10 +4,21 @@ class MyLinkedList {
       value: value,
       next: null,
     };
-    this.teal = this.head;
+    this.tail = this.head;
     this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.head.next = newNode;
+    this.tail = newNode;
+    this.length++;
   }
 }
 
 const myLinkedList = new MyLinkedList(1);
+myLinkedList.append(2);
 console.log(myLinkedList);
