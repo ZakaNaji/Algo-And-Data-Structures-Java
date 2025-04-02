6aa47a679db5ee02f77754dc8bb97cb64576f09f
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-04-02 14:38:57 +0000

    #DoublyLinkedList: append(value) - edge case : empty list -> just make head & tail point to the new node - tail.next -> newNode, newNode.prev -> tail then move tail = newNode.

--- src/main/java/com/znaji/datastructures/doublylinkedlist/Main.java
+++ src/main/java/com/znaji/datastructures/doublylinkedlist/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList dll = new DoublyLinkedList(1);
 
+        dll.append(2);
+
         System.out.println(dll);
     }
 }
