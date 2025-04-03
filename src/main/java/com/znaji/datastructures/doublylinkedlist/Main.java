823c7a08825efdf6b8e9570d98363405fc1e7d57
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-04-03 12:25:24 +0000

    #DoublyLinkedList: insert(index, value) - edge cases: index<0 || index>length => false - index = 0 => prepend(value) - index = length => append(value) - before= get(index-1), after = before.next - newNode.prev=before, newNode.next=after -before.next=newNode, afetr.prev=newNode

--- src/main/java/com/znaji/datastructures/doublylinkedlist/Main.java
+++ src/main/java/com/znaji/datastructures/doublylinkedlist/Main.java
@@ -2,13 +2,12 @@ package com.znaji.datastructures.doublylinkedlist;
 
 public class Main {
     public static void main(String[] args) {
-        DoublyLinkedList dll = new DoublyLinkedList(10);
-        dll.append(20);
-        dll.append(30);
-
-        System.out.println(dll.set(1, 99)); // true
-        System.out.println(dll.get(1).getValue()); // 99
-
-        System.out.println(dll.set(5, 100)); // false (index out of bounds)
+        DoublyLinkedList dll = new DoublyLinkedList(1);
+        dll.append(2);
+        dll.append(3);
+        dll.insert(1, 99); // should be: 1 <-> 99 <-> 2 <-> 3
+        dll.insert(0, 0);  // should be: 0 <-> 1 <-> ...
+        dll.insert(5, 4);  // should append at end: ... <-> 4
+        System.out.println(dll);
     }
 }
