3a3cba5691c79c1d3329c6837743fe6068e2bbe8
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-05-09 19:40:11 +0100

    #HashTable: find duplicates in array - loop array and store each element as key and value is number of occurences. - return an arraylist containing keys with values(num of occurences) > 1

--- src/main/java/com/znaji/datastructures/hashtable/Main.java
+++ src/main/java/com/znaji/datastructures/hashtable/Main.java
@@ -1,11 +1,12 @@
 package com.znaji.datastructures.hashtable;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        int [] array1 = new int[] {1,2,3};
-        int [] array2 = new int[] {3,4,6};
+        int [] array1 = new int[] {1,2,3,2,2,1,5,4,3};
 
-        System.out.println(itemsInCommon(array1, array2));
+        System.out.println(findDuplicates2(array1));
 
     }
 
@@ -18,5 +19,33 @@ public class Main {
             if (hashTable.get(String.valueOf(i)) != null) return true;
         }
         return false;
+    }
+
+    public static List<Integer> findDuplicates(int [] array) {
+        HashTable hashTable = new HashTable();
+        Set<Integer> duplicates = new HashSet<>();
+
+        for (int value : array) {
+            if (hashTable.get(String.valueOf(value)) != null) {
+                duplicates.add(value);
+            } else {
+                hashTable.set(String.valueOf(value), value);
+            }
+        }
+        return duplicates.stream().toList();
+    }
+
+    public static List<Integer> findDuplicates2(int array[]) {
+        Map<Integer, Integer> values = new HashMap<>();
+        for (int value : array) {
+            values.put(value, values.getOrDefault(value, 0) + 1);
+        }
+        final List<Integer> duplicates = new ArrayList<>();
+        values.forEach((key, value) -> {
+            if (value > 1) {
+                duplicates.add(key);
+            }
+        });
+        return duplicates;
     }
 }
