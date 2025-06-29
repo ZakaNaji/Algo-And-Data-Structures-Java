b36a09217a9712d8fb6273f0431c31a50555bbe8
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-06-29 20:32:15 +0100

    LeetCode : 234. Palindrome Linked List

---
+++ src/main/java/com/znaji/datastructures/linkedlist/leetcode/Solution.java
@@ -0,0 +1,34 @@
+package com.znaji.datastructures.linkedlist.leetcode;
+
+public class Solution {
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode start = head;
+        ListNode reverse = reverse(slow);
+        while (reverse != null) {
+            if (reverse.val != start.val) return false;
+            reverse = reverse.next;
+            start = start.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+}
