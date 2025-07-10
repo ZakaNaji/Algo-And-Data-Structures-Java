f92f9f93a0be81790ff16dc635e51f036929a733
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-07-10 12:33:34 +0100

    double linked prepend, removeFirst, get, set, insert, remove

--- src/main/java/com/znaji/datastructures/doublylinkedlist/refresher/DLL.java
+++ src/main/java/com/znaji/datastructures/doublylinkedlist/refresher/DLL.java
@@ -7,9 +7,9 @@ public class DLL {
     int length;
 
     public class Node {
-        public int value;
-        public Node next;
-        public Node prev;
+        private int value;
+        private Node next;
+        private Node prev;
 
         public Node(int value) {
             this.value = value;
@@ -44,6 +44,103 @@ public class DLL {
             tail.next = null;
             temp.prev = null;
         }
+        length--;
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = temp.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp;
+        if (index < length / 2) {
+            temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node node = get(index);
+        if (node == null) return false;
+        node.value = value;
+        return true;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+
+        Node newNode = new Node(value);
+        Node node = get(index);
+        newNode.prev = node.prev;
+        newNode.next = node;
+        node.prev.next = newNode;
+        node.prev = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index > length - 1) {
+            return null;
+        }
+
+        if (index == 0) {
+            return removeFirst();
+        }
+
+        if (index == length-1) {
+            return removeLast();
+        }
+
+        Node temp = get(index);
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
+        temp.next = null;
+        temp.prev = null;
         length--;
         return temp;
     }
