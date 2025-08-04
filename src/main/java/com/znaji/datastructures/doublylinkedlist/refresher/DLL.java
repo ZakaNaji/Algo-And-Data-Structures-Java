9db4b282d2b764b228650b57c088fbc7069589da
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-04 22:45:59 +0100

    double linked : reverse a doubly linked list

--- src/main/java/com/znaji/datastructures/doublylinkedlist/refresher/DLL.java
+++ src/main/java/com/znaji/datastructures/doublylinkedlist/refresher/DLL.java
@@ -152,4 +152,18 @@ public class DLL {
         tail.value = tempValue;
 
     }
+
+    public void reverse() {
+        Node current = head;
+        Node temp = null;
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+        temp = head;
+        head = tail;
+        tail = temp;
+    }
 }
