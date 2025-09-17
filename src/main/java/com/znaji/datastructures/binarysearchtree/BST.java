3083e550bfd105a9ee33a2dc08e56cb172e1da73
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-09-17 18:34:01 +0100

    Binary search tree: insert(no duplicates, will return false in case of duplicates)

--- src/main/java/com/znaji/datastructures/binarysearchtree/BST.java
+++ src/main/java/com/znaji/datastructures/binarysearchtree/BST.java
@@ -7,8 +7,35 @@ public class BST {
         int value;
         Node left;
         Node right;
+
         Node(int value) {
             this.value = value;
         }
     }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node current = root;
+        while (true) {
+            if (current.value == value) return false;
+            if (current.value > value) {
+                if (current.left == null) {
+                    current.left = newNode;
+                    return true;
+                }
+                current = current.left;
+            } else {
+                if (current.right == null) {
+                    current.right = newNode;
+                    return true;
+                }
+                current = current.right;
+            }
+        }
+    }
 }
+
