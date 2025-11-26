1d1faa37892646b033d8420c5df58ba9c8165ede
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-11-26 19:13:55 +0100

    Leetcode - 560. Subarray Sum Equals K

--- src/main/java/com/znaji/datastructures/hashtable/TwoSum.java
+++ src/main/java/com/znaji/datastructures/hashtable/TwoSum.java
@@ -19,11 +19,13 @@ public class TwoSum {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(subarraySum(new int[] {2,3,4,5,1}, 9)));
+        System.out.println(subarraySum(new int[] {1,1,1},2));
+        System.out.println(subarraySum(new int[] {1,2,3},3));
+        System.out.println(subarraySum(new int[] {1,2,1,2,1},3));
 
     }
 
-    public static int[] subarraySum(int [] nums, int target) {
+    public static int[] subarraySum1(int [] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
         int sum = 0;
@@ -36,5 +38,21 @@ public class TwoSum {
             map.put(sum, i);
         }
         return new int[2];
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixMap = new HashMap<>();
+        prefixMap.put(0, 1);
+        int sum = 0;
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int rest = sum - k;
+            if (prefixMap.containsKey(rest)) {
+                total += prefixMap.get(rest);
+            }
+            prefixMap.compute(sum, (key,v) -> (v == null)? 1 : v+1);
+        }
+        return total;
     }
 }
