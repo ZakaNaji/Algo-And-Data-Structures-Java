18438468577bc6c6522ebe3c7277ed2a00ab1d81
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-02-11 15:12:42 +0100

    improved insertion sort; switch based instead of swaping each iteration

--- src/main/java/com/znaji/algorithms/sort/insertionsort/InsertionSort.java
+++ src/main/java/com/znaji/algorithms/sort/insertionsort/InsertionSort.java
@@ -15,14 +15,13 @@ public class InsertionSort {
 
     public static void insertionSort(int [] array) {
         for (int i = 1; i < array.length; i++) {
-            int unsortedIndex = i;
-            for (int j = i-1; j >=0; j--) {
-                if (array[j] <= array[unsortedIndex]) {
-                    break;
-                }
-                swap(array, unsortedIndex, j);
-                unsortedIndex = j;
+            int inserted = array[i];
+            int j = i;
+            while (j>0 && array[j-1] > inserted) {
+                array[j] = array[j-1];
+                j--;
             }
+            array[j] = inserted;
         }
     }
 }
