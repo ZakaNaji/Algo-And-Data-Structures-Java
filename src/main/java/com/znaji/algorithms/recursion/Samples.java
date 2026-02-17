90648b8181d591accfa26d2cc00dde2fb9a3f758
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-02-17 18:49:03 +0000

    Fibonacci sequence recursively

--- src/main/java/com/znaji/algorithms/recursion/Samples.java
+++ src/main/java/com/znaji/algorithms/recursion/Samples.java
@@ -4,10 +4,18 @@ public class Samples {
     public static void main(String[] args) {
         int factorial = factorialOf(5);
         System.out.println("factorial of 5: " + factorial);
+
+        int fibonacciedSequence = fibonacciSequence(40);
+        System.out.println("Fibonacci sequence: " + fibonacciedSequence);
     }
 
     private static int factorialOf(int num) {
         if (num == 1) return 1;
         return factorialOf(num-1) * num;
+    }
+
+    private static int fibonacciSequence(int n) {
+        if (n < 2) return n;
+        return fibonacciSequence(n-1) + fibonacciSequence(n-2);
     }
 }
