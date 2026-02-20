29c6f4127a3de9df99d866e6406b49e7f7b91f01
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-02-20 13:54:39 +0000

    Binary Tree : insert

---
+++ src/main/java/com/znaji/datastructures/binarysearchtree/MyBinarySearchTree.java
@@ -0,0 +1,47 @@
+package com.znaji.datastructures.binarysearchtree;
+
+public class MyBinarySearchTree {
+    private Node root;
+
+    public MyBinarySearchTree() {
+    }
+
+    public MyBinarySearchTree(int value) {
+        this.root = new Node(value);
+    }
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new Node(value);
+            return;
+        }
+        Node temp = root;
+        while (true) {
+            if (value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = new Node(value);
+                    return;
+                } else {
+                    temp = temp.left;
+                }
+            } else {
+                if (temp.right == null) {
+                    temp.right = new Node(value);
+                    return;
+                } else {
+                    temp = temp.right;
+                }
+            }
+        }
+    }
+
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
